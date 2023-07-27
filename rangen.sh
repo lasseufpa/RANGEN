dba42b34519687234f45740bd435d0a3be529cdc
HeitorAnglada <heitoranglada@gmail.com>
2023-07-27 16:58:02 -0300

    build: add docker-compose-base file

--- rangen.sh
+++ rangen.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 
+# Input parameters for the number of UEs and gNBs
 number_ues=$1
 number_gnb=$2
+
+
 nome_arquivo="docker-compose-setup.yaml"
 imsi=208930000000000
 imsi_f=208930000000000
 link_gnb="gnb.free5gc.org"
 
+# Copy the contents of the base file to the setup file to ensure a known initial state
+cp docker-compose-base.yaml docker-compose-setup.yaml
 
-touch ./ueransim/start.sh
-
-start_file="
-#!/bin/bash
-
-num_ue=$1
-tot_ues=$2
-
-# Iniciar o comando ./nr-ue em segundo plano
-./nr-ue -c ./config/uecfg${num_ue}.yaml -n ${tot_ues} &
-
-sleep 40
-
-# Loop para executar o ping em cada interface uesimtunX
-for ((l=0; l<${tot_ues}; l++));
-do
-  interface="uesimtun$l"
-  ping google.com -I ${interface} -i 0.05 -n &
-done
-
-# Manter o script em execução para que o contêiner não seja encerrado
-tail -f /dev/null
-"
-
-echo "$start_file" >> ./ueransim/start.sh
-
-#copia o conteúdo do arquivo build para o setuup para garantir um esado inicial conhecido
-cp docker-compose-build.yaml docker-compose-setup.yaml
-
-#a definição de redes para ser add no final do arquivo de setup
+# Network definitions to be added at the end of the setup file
 final="
 networks:
   privnet:
@@ -52,9 +28,10 @@ volumes:
   dbdata:
 "
 
-#bloco que add um contâiner ppara cada pack de gnb+ues
+# Loop to add a container for each gNB+UEs pack
 for ((i=0; i<$number_gnb; i++));
 do
+    # Block that defines the service for each UE and gNB
     service_block="
   ueransim$i:
     container_name: ueransim$i
@@ -98,33 +75,33 @@ do
       - free5gc-upf
     command: ./nr-gnb -c ./config/gnbcfg$i.yaml &
 "
-#add o bloco acima no docker compose
+# Append the service block to the docker-compose file
 echo "$service_block" >> "$nome_arquivo"
 
-#cria um arquivo uecfgx com base no arquivo uecfg que começa no imsi de final 0
-#sendo x o num atual da gnb
+# Create a uecfgx file based on the uecfg file starting with an imsi of 0
+# Here x is the current number of gNB
 cat config/uecfg.yaml > config/uecfg$i.yaml
 
-#calcula o imsi com base no arquivo de configuracão(num da gnb)
+# Calculate the imsi based on the configuration file (number of gNB)
 sed -i "s#$imsi#$imsi_f#g" config/uecfg$i.yaml
 imsi_f=$(( $number_ues * ($i + 1) + $imsi ))
 
-#troca o ip da rede no arquivo da ue com base na gnb
+# Change the network IP in the UE file based on the gNB
 sed -i "s#$link_gnb#gnb$i.free5gc.org#g" config/uecfg$i.yaml
 
-#cria um arquivo gnbcfgx com base no arquivo gnbcfg que começa com ip gnb.free5gc.org
-#sendo x o num atual da gnb
+# Create a gnbcfgx file based on the gnbcfg file starting with IP gnb.free5gc.org
+# Here x is the current number of gNB
 cat config/gnbcfg.yaml > config/gnbcfg$i.yaml
 
-#troca o ip da rede no arquivo da gnb com base no num da gnb
+# Change the network IP in the gNB file based on the gNB number
 sed -i "s#$link_gnb#gnb$i.free5gc.org#g" config/gnbcfg$i.yaml
 
 done
 
-#adciona as configurações de rede no final do arquivo
+# Add network configurations at the end of the file
 echo "$final" >> "$nome_arquivo"
 
-#buid os containers do ueransim
+# Build the ueransim containers
 for ((j=0; j<$number_gnb; j++));
 do
 docker compose -f docker-compose-setup.yaml build ueransim$j
@@ -133,11 +110,11 @@ done
 
 sleep 5
 
+# Start the services
 docker compose -f docker-compose-setup.yaml up
 
 
-#deleta os arquivos de config das ues
-
+# At the end of the script execution, delete the generated configuration files.
 for ((k=0; k< $number_gnb; k++));
 do
 
@@ -145,7 +122,9 @@ file_name="config/uecfg$k.yaml"
 rm -rf "${file_name}"
 
 file_name="config/gnbcfg$k.yaml"
-rm -rf "${file_name}"    
+rm -rf "${file_name}"   
 done
 
-#sed -i "s#$1#numero_ues#g" ueransim/start.sh
+file_name="docker-compose-setup.yaml"
+rm -rf "${file_name}"
+
